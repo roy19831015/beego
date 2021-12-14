baccbebf1c53304b701d32c147669ab2f95c6afb
roy19831015@gmail.com <roy19831015@gmail.com>
2021-12-14 11:22:57 +0800

    1st

--- newGitRelease.sh
+++ newGitRelease.sh
@@ -1,10 +1,10 @@
-echo "git commit -a -m '第v3.0.$1版本'"
-git commit -a -m "第v3.0.$1版本"
-echo "git checkout -b release/v3.0.$1"
-git checkout -b release/v3.0.$1
-echo "git push -u origin release/v3.0.$1"
-git push -u origin release/v3.0.$1
-echo "git tag v3.0.$1"
-git tag v3.0.$1
+echo "git commit -a -m '第v2.0.$1版本'"
+git commit -a -m "第v2.0.$1版本"
+echo "git checkout -b release/v2.0.$1"
+git checkout -b release/v2.0.$1
+echo "git push -u origin release/v2.0.$1"
+git push -u origin release/v2.0.$1
+echo "git tag v2.0.$1"
+git tag v2.0.$1
 echo "git push --tags"
 git push --tags
